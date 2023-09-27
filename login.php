dee8a62f50ca5af6390f6f883cf5796e362c669b
alfian <alfianmusthofa45264@gmail.com>
2023-09-27 13:56:54 +0700

    cek

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <script src="https://kit.fontawesome.com/707c864a21.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?= 'css/index.css'; ?>"> EMANG GINI ?
+</head>
+
+<body>
+    <section class="container">
+        <form action="" method="post">
+            <div class="col">
+                <img src="assets/loginimage.jpg" alt="lah">
+            </div>
+            <div class="col">
+                <div class="title">
+                    <h1>Hello! Friend!</h1>
+                </div>
+                <div class="input-field">
+                    <p>Username*</p>
+                    <input type="text" name="username" placeholder="Enter your Username" autocomplete="OFF">
+                </div>
+                <div class="input-field">
+                    <p>Password*</p>
+                    <input type="password" name="password" placeholder="Enter your Password">
+                </div>
+            </div>
+        </form>
+    </section>
+</body>
+
+</html>
