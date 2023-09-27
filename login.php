dc2222acb2877668f7323f9ade9943580d1936e6
alfian <alfianmusthofa45264@gmail.com>
2023-09-27 16:08:39 +0700

    alfian

--- login.php
+++ login.php
@@ -6,26 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <script src="https://kit.fontawesome.com/707c864a21.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="<?= 'css/index.css'; ?>"> EMANG GINI ?
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
     <section class="container">
         <form action="" method="post">
             <div class="col">
-                <img src="assets/loginimage.jpg" alt="lah">
+                <img src="assets/bg.jpg">
             </div>
             <div class="col">
-                <div class="title">
-                    <h1>Hello! Friend!</h1>
-                </div>
-                <div class="input-field">
-                    <p>Username*</p>
-                    <input type="text" name="username" placeholder="Enter your Username" autocomplete="OFF">
-                </div>
-                <div class="input-field">
-                    <p>Password*</p>
-                    <input type="password" name="password" placeholder="Enter your Password">
+                <div class="caption">
+                    <div class="title">
+                        <h1>Hello! Friend!</h1>
+                    </div>
+                    <div class="input-field">
+                        <p>Username*</p>
+                        <input type="text" name="username" placeholder="Enter your Username" autocomplete="OFF">
+                    </div>
+                    <div class="input-field">
+                        <p>Password*</p>
+                        <input type="password" name="password" placeholder="Enter your Password">
+                    </div>
+                    <div class="cookie">
+                        <input type="checkbox" name="ingatsaya" id="ingatsaya" class="check">
+                        <p>Ingat saya</p>
+                    </div>
+                    <button type="submit" name="login">LOGIN</button>
+                    <div class="register">
+                        <a href="#">Lupa password?</a>
+                        <a href="#">Tidak punya akun?</a>
+                    </div>
                 </div>
             </div>
         </form>
