eb75153331017db7dca38b8390840e3825878d19
alfian <alfianmusthofa45264@gmail.com>
2023-09-28 11:38:45 +0700

    fixcan

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TokoEdi | Meyediakan Berbagai kebutuhan</title>
+    <link rel="stylesheet" href="css/index.css">
+    <script src="https://kit.fontawesome.com/707c864a21.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <section class="header">
+        <div class="logo">
+            <a href="#">OurShop</a>
+        </div>
+        <div class="navlink">
+            <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+            <a href="login.php"><i class="fa-solid fa-user"></i></a>
+        </div>
+        <div class="menu">
+            <a href="#" class="menu"><i class="fa-solid fa-bars"></i></a>
+        </div>
+        <div class="nav-menu">
+            <div class="submenu">
+                <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                <p>Keranjang</p>
+            </div>
+            <div class="submenu">
+                <a href="login.php"><i class="fa-solid fa-user"></i></a>
+                <p>Masuk</p>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
